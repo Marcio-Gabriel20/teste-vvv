5286bdb4e1fd19d10fd96a68f81c245e7ae7c872
Marcio Gabriel <gabriel09msn@gmail.com>
2023-04-07 21:55:03 -0300

    Update

--- TesteVVV/testevvv/src/main/java/com/vvvtest/testevvv/controller/ModalController.java
+++ TesteVVV/testevvv/src/main/java/com/vvvtest/testevvv/controller/ModalController.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,7 +36,7 @@ public class ModalController {
 
     }
 
-    @PatchMapping("/{id}/status")
+    @PutMapping("/{id}/status")
     public Modal atualizaModal(@PathVariable Long id, @RequestParam String status) {
 
         return modalService.atualizaModal(id, status);
